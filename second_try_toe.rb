7d8f3a6c6369387a5b6284fa6fdb4b02360475d7
Daniel Murphy <danielmichaelmurphy@gmail.com>
2020-08-17 23:16:55 -0400

    Working no-thrills ruby tic-tac-toe game

--- second_try_toe.rb
+++ second_try_toe.rb
@@ -11,7 +11,7 @@
     # Replace empty value with marker value
   
   # Position Check
-    # Check is poisition is available/empty
+    # Check if poisition is available/empty
   
   # Mark
     # Use placeholder/input value and marker value
@@ -141,26 +141,159 @@ class Board
 
 end
 
-tic_tac_toe = Board.new
+############################
 
-tic_tac_toe.display_board
+# Pesudo Code for Player Class
 
-tic_tac_toe.mark_position(4, 'X')
+# Player
 
-tic_tac_toe.display_board
+  # Initialize Player
+    # Set 2 player variables
+    # Get names and assign to the player variables
+    # Assign markers to players
+    # Puts player their own marker
+  
+  # Current Player Swap
+    # Default to player 1 (Maybe goes in initialize?)
+    # When called, swap player for other player
+  
+  # Get pick
+    # Start loop
+      # Ask current player for their pick (display updated board first?)
+      # Validate pick format
+      # Call Board class for mark method
+      # End loop if position available
+    # Call Current Player Swap
+  
+    
+############################
 
-tic_tac_toe.full_check
+class Player
+  #Players plural?
 
-tic_tac_toe.mark_position(0, 'X')
-tic_tac_toe.mark_position(1, 'X')
-tic_tac_toe.mark_position(2, 'X')
-tic_tac_toe.mark_position(3, 'X')
-tic_tac_toe.mark_position(5, 'X')
-tic_tac_toe.mark_position(6, 'X')
-tic_tac_toe.mark_position(7, 'X')
-tic_tac_toe.full_check
-tic_tac_toe.mark_position(8, 'X')
+  def initialize
+    @player1 = 'X'
+    @player2 = 'O'
+    # Assign markers to players
+    # Puts player their own marker
+    # Default current player to player 1
+    @current_player = @player1
+    #@pick = ''
+  end 
+  
+  def player_swap
+    # When called, swap player for other player
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end    
+  end
+  
+  def get_current_player_pick
+    # Start pick number loop
+    loop do
+    # Ask current player for their pick (display updated board first?)
+      puts "#{@current_player} pick an open number: "
+      @pick = gets.chomp.to_i
+      # Validate pick format
+      if @pick.between?(1,9)
+        puts 'Valid format, checking for availability...'
+        # Break pick number loop
+        break
+      end
+    end
+    @pick
+  end
 
-tic_tac_toe.display_board
-tic_tac_toe.full_check
-tic_tac_toe.win_check
+  def show_current_player
+    @current_player
+  end
+
+end
+   #def call_board_check
+
+    #  if @board.position_check(@pick)
+     #   break
+      #end
+    # End loop if position available
+   # end
+    # Call Current Player Swap
+    #player_swap
+ # end
+
+class Game
+  def initialize
+    @board = Board.new
+    @player = Player.new
+  end
+
+  def start_game
+    #loop do
+      until end_game
+        @board.display_board  
+        current_player_pick = @player.get_current_player_pick
+        current_player_position = current_player_pick - 1
+        puts current_player_pick
+        current_player = @player.show_current_player 
+        @board.mark_position(current_player_position, current_player)
+        @player.player_swap
+      end
+    #end
+  end
+
+  def end_game
+    if @board.full_check || @board.win_check
+      true
+    end
+  end
+
+end
+
+  tic_tac_toe = Game.new
+  tic_tac_toe.start_game
+  
+
+  #tic_tac_toe = Board.new
+
+  #tic_tac_toe.display_board
+  
+  #tic_tac_toe.mark_position(4, 'X')
+  
+  #tic_tac_toe.display_board
+  
+  #tic_tac_toe.full_check
+  
+  #tic_tac_toe.mark_position(0, 'X')
+  #tic_tac_toe.mark_position(1, 'X')
+  #tic_tac_toe.mark_position(2, 'X')
+  #tic_tac_toe.mark_position(3, 'X')
+  #tic_tac_toe.mark_position(5, 'X')
+  #tic_tac_toe.mark_position(6, 'X')
+  #tic_tac_toe.mark_position(7, 'X')
+  #tic_tac_toe.full_check
+  #tic_tac_toe.mark_position(8, 'X')
+  
+  #tic_tac_toe.display_board
+  #tic_tac_toe.full_check
+  #tic_tac_toe.win_check
+
+
+#test = Player.new
+#test.get_current_player_pick
+#test.player_swap
+#test.get_current_player_pick
+
+# How do I use one of the Board class methods from within the Player class method?
+
+#You have to start your loop. So you'll need something that gets that going from the CL. That
+# would essentially start the Game. Then the game could initialize the Players. Then the Players would
+# know the data of the player names and markers. 
+
+# a Player has-a Pick
+
+
+
+### So running game, board and players. Loop starts and can get pick and can check and mark pick.
+### Situation: I want my game start loop to swap player turns back and forth. It's stuck doing
+### Player 1, Player 2, Player 2, Player 2, etc.
